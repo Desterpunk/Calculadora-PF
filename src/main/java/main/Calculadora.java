dec45abbbd60050a11597ef39a2c35878df9742d
Jhon Serna <jhonalfaber@gmail.com>
2021-10-06 09:25:17 -0500

    solucionando la multiplicacion de numeros negativos, y la division por 0

--- src/main/java/main/Calculadora.java
+++ src/main/java/main/Calculadora.java
@@ -7,15 +7,20 @@ public class Calculadora {
     public static void main(String[] args) {
         BiFunction<Integer,Integer,Integer> add = (a,b) -> a + b;
         BiFunction<Integer,Integer,Integer> substraction = (a,b) -> a - b;
-        BiFunction<Integer,Integer,Integer> multiplication = (a,b) -> IntStream.range(0, b+1)
-                        .reduce((accumulator, number) -> add.apply(accumulator + a , 0)).getAsInt();
-        BiFunction<Integer,Integer,Integer> division = (a,b) -> IntStream.range(0, a+1)
-                        .reduce((accumulator,number) -> multiplication.apply(number,b) <= a ? add.apply(accumulator,1) : accumulator).getAsInt();
+        BiFunction<Integer,Integer,Integer> multiplication = (a,b) -> IntStream.range(0, Math.abs(b)+1)
+                        .reduce((accumulator, number) -> b<=0 ? add.apply(accumulator-a,0) : add.apply(accumulator + a, 0)).getAsInt();
+
+        BiFunction<Integer,Integer,Integer> division = (a,b) -> IntStream.range(0, Math.abs(a)+1)
+                        .reduce((accumulator,number) ->
+                                multiplication.apply(a,b) == 0 ? accumulator = 0:
+                                multiplication.apply(a,b) < 0 ? multiplication.apply(number,Math.abs(b)) <= Math.abs(a) ? add.apply(accumulator,-1) : accumulator :
+                                multiplication.apply(number,Math.abs(b)) <= Math.abs(a) ? add.apply(accumulator,1) : accumulator).getAsInt();
+
 
         System.out.println("Calculator functional");
         System.out.println("Summa: " + add.apply(5,5));
         System.out.println("Subtraction: " + substraction.apply(10,3));
-        System.out.println("Multiplication: " + multiplication.apply(10,3));
-        System.out.println("Division: " + division.apply(100,5));
+        System.out.println("Multiplication: " + multiplication.apply(10,-10));
+        System.out.println("Division: " + division.apply(-10,5));
     }
 }
